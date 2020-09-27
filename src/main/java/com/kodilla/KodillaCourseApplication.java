5bd7b569499a98e4e7626d59c8aea612a762a502
Adam Stankiewicz <as1993@o2.pl>
2020-09-27 23:24:11 +0200

    First Commit

--- src/main/java/com/kodilla/KodillaCourseApplication.java
+++ src/main/java/com/kodilla/KodillaCourseApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KodillaCourseApplication {
-
     public static void main(String[] args) {
+        System.out.print("Kodilla");
         SpringApplication.run(KodillaCourseApplication.class, args);
     }
 
