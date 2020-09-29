f75408d20c4419ad49c8fa5a1f9a6836a77c2d66
Adam Stankiewicz <as1993@o2.pl>
2020-09-29 16:56:54 +0200

    Changes

--- kodilla - testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla - testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.testing;
 
 public class git TestingMain {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
     }
+}
