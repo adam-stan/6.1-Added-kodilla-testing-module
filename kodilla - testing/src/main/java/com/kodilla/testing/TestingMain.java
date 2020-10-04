ff529c0798f2426b500dd6a6363e7424f68a0c9b
Adam Stankiewicz <as1993@o2.pl>
2020-10-04 20:15:59 +0200

    Task6.3

--- kodilla - testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla - testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -9,26 +9,6 @@ import java.sql.SQLOutput;
 
 public class TestingMain {
     public static void main(String[] args){
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")){
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-
-        Calculator calculate = new Calculator();
-
-        int sumingAB = calculate.summing(15, 10);
-        int substractingAB = calculate.substracting(15, 10);
-
-        if (sumingAB == 25 && substractingAB == 5) {
-            System.out.println("Calculator test is correct.");
-        } else {
-            System.out.println("Error");
-        }
     }
 }
---
+++ kodilla - testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,63 @@
+package com.kodilla.testing.collection;
+import java.util.ArrayList;
+import org.junit.jupiter.api.*;
+
+class CollectionTestSuite {
+    @BeforeEach
+    public void before(){
+        System.out.println("Test Case: begin");
+    }
+
+    @AfterEach
+    public void after(){
+        System.out.println("Test Case: end");
+    }
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Test Suite: begin");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("Test Suite: end");
+    }
+
+    @DisplayName("when we create emptyList, " +
+            "then this emptyList should be equals to resultList"
+    )
+    @Test
+    void testOddNumbersExterminatorEmptyList() {
+        //Given
+        ArrayList<Integer> emptyList = new ArrayList<Integer>();
+        //When
+        ArrayList<Integer> resultList = OddNumbersExterminator.exterminate(emptyList);
+        System.out.println("Now we are testing EmptyList");
+        //Then
+        Assertions.assertEquals(emptyList, resultList);
+
+    }
+
+    @DisplayName("when we create evenAndUnevenNumbersList and call a method exterminate() on it" +
+            "then this list should be equals to evenNumbersList."
+    )
+    @Test
+    void testCaseUsername() {
+        //Given
+        ArrayList<Integer> evenAndUnevenNumbersList = new ArrayList<>();
+        evenAndUnevenNumbersList.add(5);
+        evenAndUnevenNumbersList.add(10);
+        evenAndUnevenNumbersList.add(12);
+        evenAndUnevenNumbersList.add(17);
+
+        ArrayList<Integer>evenNumbersList = new ArrayList<>();
+        evenNumbersList.add(10);
+        evenNumbersList.add(12);
+
+        //When
+        ArrayList<Integer> resultList = OddNumbersExterminator.exterminate(evenAndUnevenNumbersList);
+        System.out.println("Now we are testing evenAndUnevenNumbersList");
+        //Then
+        Assertions.assertEquals(evenNumbersList, resultList);
+    }
+}
