d989917f5a9939ac25b5a2e81a20f6193f5c239f
Adam Stankiewicz <as1993@o2.pl>
2020-11-19 22:07:54 +0100

    Task13.1

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearching.java
@@ -0,0 +1,27 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+public class FlightSearching {
+
+
+    public Map<String, Boolean> findFlight(Flight flight){
+
+
+        Map<String, Boolean> canFly = new HashMap<>();
+
+        canFly.put("Warsaw", true);
+        canFly.put("Posen", false);
+        canFly.put("Krakow", true);
+        canFly.put("Gdansk", false);
+        canFly.put("Lodz", true);
+
+        if(canFly.entrySet())
+
+        return canFly.get(String)
+    }
+
+}
+*/
